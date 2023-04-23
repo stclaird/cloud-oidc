6a9b61ae2e4b1f5c79fcd5cf6138d6c90585d00d
David StClair <david.stclair@davidstclair.co.uk>
2023-04-23 16:56:22 +0100

    Adding commits

--- github-to-aws/create-oidc.bash
+++ github-to-aws/create-oidc.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
-while getopts a:r: flag
+while getopts a:r:n flag
 do
     case "${flag}" in
         a) accountid=${OPTARG};;
         r) repo=${OPTARG};;
+		n) name=${OPTARG};;
     esac
 done
 
@@ -19,14 +20,18 @@ then
 	exit 1
 fi
 
+if [ -z "$name" ]
+then 
+name="github-oidc"
+fi
+
 export AWS_ACCOUNT_ID=$accountid
 export REPO_VALUE=$repo
 
+ROLE_NAME=$name
+
 openssl s_client -servername token.actions.githubusercontent.com -showcerts -connect token.actions.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
-
 THUMBLIST=$(openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]')
-
-echo "$THUMBLIST"
 
 OPENIDPROVIDERARN=$(aws iam create-open-id-connect-provider \
 	--url https://token.actions.githubusercontent.com \
@@ -35,9 +40,6 @@ OPENIDPROVIDERARN=$(aws iam create-open-id-connect-provider \
 	| jq -r .OpenIDConnectProviderArn)
 
 envsubst < trust-policy.json.tmpl > trust-policy.json
-
-aws iam create-role --role-name github-actions --assume-role-policy-document file://trust-policy.json > /dev/null
-
-ROLE_ARN=$(aws iam get-role --role-name github-actions | jq -r '.Role .Arn')
-
-echo "Use this ARN in your github action's role-to-assume: ${ROLE_ARN}"
+aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file://trust-policy.json > /dev/null
+ROLE_ARN=$(aws iam get-role --role-name  ${ROLE_NAME} | jq -r '.Role .Arn')
+echo "This is the role-to-assume: ${ROLE_ARN}"
