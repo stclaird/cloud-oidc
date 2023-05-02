b2aecd2614d9211687aa9bea8f5f86900b66690c
David StClair <david.stclair@davidstclair.co.uk>
2023-05-02 17:17:04 +0100

    better options

--- github-to-aws/create-oidc.bash
+++ github-to-aws/create-oidc.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts a:r:n flag
+while getopts :a:r:n: flag
 do
     case "${flag}" in
         a) accountid=${OPTARG};;
@@ -7,6 +7,8 @@ do
 		n) name=${OPTARG};;
     esac
 done
+
+
 
 if [ -z "$accountid" ]
 then 
@@ -22,13 +24,15 @@ fi
 
 if [ -z "$name" ]
 then 
-name="github-oidc"
+	echo "Please supply role name (-n github-oidc )"
+	exit 1
 fi
-
 export AWS_ACCOUNT_ID=$accountid
 export REPO_VALUE=$repo
 
-ROLE_NAME=$name
+export ROLE_NAME=$name
+
+echo $name
 
 openssl s_client -servername token.actions.githubusercontent.com -showcerts -connect token.actions.githubusercontent.com:443 < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sed "0,/-END CERTIFICATE-/d" > certificate.crt
 THUMBLIST=$(openssl x509 -in certificate.crt -fingerprint -noout | cut -f2 -d'=' | tr -d ':' | tr '[:upper:]' '[:lower:]')
